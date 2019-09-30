145812c375bd6a7b98d38a6361cf94cd336c2c23
coolcoolbing <51466689+coolcoolbing@users.noreply.github.com>
2019-09-30 16:45:32 +0800

    compelete
    完成

---
+++ Open Other Exe Program/Assets/Scripts/OperateOtherExeProgram.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;        //需要引入命名空间
+using System.IO;
+using System;
+using System.Runtime.InteropServices;
+
+public class OperateOtherExeProgram : MonoBehaviour
+{
+    Process pro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// 打开exe程序
+    /// </summary>
+    public void OpenExe()
+    {
+        //设置exe的相对路径
+        string a = Application.dataPath;
+        a=a.Replace("Assets",string.Empty);                //剔除掉Assets，在与Assets文件夹同级的目录下寻找
+        string path = a+"AudioControl/AudioControl.exe";  //需要打开的exe的路径，根据实际填写
+
+        print(path);
+        if (!File.Exists(path)) { UnityEngine.Debug.LogError("没有找到该文件");return; }
+
+        //if (pro!=null) { UnityEngine.Debug.LogWarning("静音程序已经打开，请勿重复操作"); return; }
+        pro = new Process();
+        pro.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Minimized;     //把窗口最小化，使其在后台运行。也可以选择把窗口隐藏。
+        pro.StartInfo.FileName = path;       //设置要打开的exe程序的路径
+        pro.Start();       //启动exe程序 
+    }
+
+    /// <summary>
+    /// 关闭exe程序
+    /// </summary>
+    public void CloseExe()
+    {
+        if (pro != null)
+        {
+            //File.WriteAllText("123.txt","close");
+            pro.Kill();                       //杀死所有的进程
+            pro.Dispose();               //释放所有的资源
+            pro.Close();                  //关闭exe程序
+            pro = null;
+        }  
+    }
+
+    public void Mute()
+    {
+        File.WriteAllText("./AudioControl/AppIsMute.txt", "true");
+    }
+
+    public void NotMute()
+    {
+        File.WriteAllText("./AudioControl/AppIsMute.txt", "false");
+    }
+}
---
+++ Open Other Exe Program/AudioControl/AudioControl/AudioControl/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CoreAudioApi;
+
+namespace AudioControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        MMDevice device;
+        AudioSessionManager audioSessionManager;
+        SessionCollection sessions;
+
+        /// <summary>
+        /// 开启程序时就执行
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //初始化设备
+            MMDeviceEnumerator devEnum = new MMDeviceEnumerator();
+            device = devEnum.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
+
+            //label1.Text = "当前音量：" + Convert.ToInt32(device.AudioEndpointVolume.MasterVolumeLevelScalar * 100.0f); 
+
+            
+            //获取所有程序声音的管理器
+            audioSessionManager = device.AudioSessionManager;
+            sessions = audioSessionManager.Sessions;
+
+            //显示所有程序的SessionIdentifier
+            label1.Text ="当前应用音量的个数"+ audioSessionManager.Sessions.Count+
+                "。当前所有音频程序的SessionIdentifier:\n";
+            for (int i = 0; i < audioSessionManager.Sessions.Count; i++)
+            {
+                AudioSessionControl app = sessions[i];
+                string sIdentifier = app.SessionIdentifier;
+                label1.Text += app.SessionIdentifier + "\n";
+            }
+
+            
+            Thread a = new Thread(CheckTxT);
+            a.IsBackground = true;
+            a.Start();
+            //CheckTxT();
+        }
+
+        //检查txt文件用来
+        private void CheckTxT()
+        {
+            while (true)
+            {
+                if (File.ReadAllText("AppIsMute.txt") == "true")
+                {
+                    /*
+                    Thread b = new Thread(()=> { FindTheApplication(textBox1.Text, true); });
+                    b.IsBackground = true;
+                    b.Start();*/
+                    FindTheApplication(textBox1.Text, true);
+                }
+                else if(File.ReadAllText("AppIsMute.txt") == "false")
+                {
+                    /*
+                    Thread b = new Thread(() => { FindTheApplication(textBox1.Text, false); });
+                    b.IsBackground = true;
+                    b.Start();*/
+                    FindTheApplication(textBox1.Text, false);
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        /// <summary>
+        /// 找到我们要找的程序
+        /// </summary>
+        /// <param name="exeName">exe程序的名字</param>
+        /// <param name="isMute">静音还是不静音</param>
+        private void FindTheApplication(string exeName,bool isMute)
+        {
+            
+            //判断这个程序是不是我们要的
+            for (int i=0;i< audioSessionManager.Sessions.Count;i++)
+            {
+                AudioSessionControl app = sessions[i];
+                string sIdentifier = app.SessionIdentifier;
+                
+                if (isTheAppWeNeed(sIdentifier,exeName))
+                {
+                    //app.SimpleAudioVolume.MasterVolume=0;
+
+                    app.SimpleAudioVolume.Mute = isMute;//设置静音状态
+                    if (isMute)
+                    {
+                        //label3.Text = "静音成功!";
+                    }
+                    else
+                    {
+                        //label3.Text = "取消静音成功!";
+                    }
+                    
+                    return;
+                }
+            }
+            //label3.Text = "没有找到该exe程序的音量控制实体";
+        }
+
+        /// <summary>
+        /// 根据identify判断是不是我们要找的程序
+        /// </summary>
+        /// <param name="sIdentifier"></param>
+        /// <returns></returns>
+        private bool isTheAppWeNeed(string sIdentifier,string exeName)
+        {
+            if (exeName== "") { return false; }
+            if(sIdentifier.Contains(exeName+ ".exe"))//判断Identifier中是不是我们需要的程序
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 静音指定的程序
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            FindTheApplication(textBox1.Text,true);
+        }
+
+        /// <summary>
+        /// 取消静音指定的程序
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            FindTheApplication(textBox1.Text, false);
+        }
+    }
+}
